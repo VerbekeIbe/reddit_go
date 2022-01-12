eaeef9ebf04fb99dc29442a9946d0d6d38b54eef
Ibe Verbeke <ibe.verbeke@student.howest.be>
2022-01-12 17:40:39 +0100

    cleanly generated users and subreddits
    Next up: posts and comments

---
+++ data/dataCleaner.py
@@ -0,0 +1,29 @@
+import uuid, json, requests
+
+# CLEANUP SUBREDDIT DATA
+def cleanup_subreddits(): 
+    with open('json/subreddits/subreddits.json', 'r') as infile:
+        file = json.load(infile)
+        subreddits = file['data']['children']
+
+        data = []
+    
+        for subreddit in subreddits:
+            subdata = {
+                'id': str(uuid.uuid4()),
+                'name' : subreddit['data']['display_name'],
+                'description': subreddit['data']['description'],
+                'description_html': subreddit['data']['description_html']
+            }
+            data.append(subdata)
+
+    with open('json/cleanup/clean_subreddits.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+
+
+
+
+#EXECUTE REGION
+    
+
